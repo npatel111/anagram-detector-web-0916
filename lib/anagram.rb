1d771e6ab9475007c598ae8f25084c66bf158221
Niti Nrw <niti.patel@flatironschool.com>
2016-09-29 14:51:48 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  #permutation method gives all combinations
+  # see if that array matches
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    all_possible_combos = @word.split("").permutation.to_a
+    #binding.pry
+    array.select {|word| all_possible_combos.include?(word.split(""))
+    }
+  end
+
+end
